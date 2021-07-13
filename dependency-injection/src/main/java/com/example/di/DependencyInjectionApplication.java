47bd450aa3a618462bf2aaffb5c33e9546804b50
armandochale <48918289+armandochale@users.noreply.github.com>
2021-07-12 22:29:52 -0500

    Add files via upload

--- dependency-injection/src/main/java/com/example/di/DependencyInjectionApplication.java
+++ dependency-injection/src/main/java/com/example/di/DependencyInjectionApplication.java
@@ -6,15 +6,35 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import com.example.di.atributo.Coche;
 import com.example.di.atributo.Motor;
+import com.example.di.qualifiers.Animal;
+import com.example.di.qualifiers.Avion;
+import com.example.di.qualifiers.Nido;
+import com.example.di.qualifiers.Pajaro;
+import com.example.di.qualifiers.Perro;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(DependencyInjectionApplication.class);
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(DependencyInjectionApplication.class, args);
 		
-		Coche coche = context.getBean(Coche.class);
-		System.out.println(coche);
+		Perro perro = context.getBean(Perro.class);
+		Pajaro pajaro = context.getBean(Pajaro.class);
+		Avion avion = context.getBean(Avion.class);
+		//Animal animal = context.getBean("pajarito",Animal.class);
+		//log.info("Animal nombre = {} edad = {}",animal.getNombre(),animal.getEdad());
+		Nido nido = context.getBean(Nido.class);
+		
+		nido.imprimir();
+		
+		
+		log.info("Objeto Perro {}",perro.getNombre());
+		avion.volar();
+		
 	}
 
 }
---
+++ dependency-injection/src/main/java/com/example/di/qualifiers/Volador.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.example.di.qualifiers;
+
+/**
+ * @author USER
+ *
+ */
+public interface Volador {
+
+	
+	public void volar();
+}

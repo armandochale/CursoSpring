950b986ea8fdfa5ba60ff85f17a9d89b8ac92304
armandochale <48918289+armandochale@users.noreply.github.com>
2021-07-12 20:20:13 -0500

    Add files via upload

---
+++ hello-world/src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.example.demo.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author USER
+ *
+ */
+@RestController
+@RequestMapping("/hello-world")
+public class HelloWorldController {
+
+	@GetMapping
+	public ResponseEntity<String> helloWorld(){
+		return new ResponseEntity<>("Hello World",HttpStatus.OK);
+	}
+}
